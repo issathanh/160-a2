4d07babb6343568ba8a5662755caff9351f34ff8
Thanh Thuong <thanh.thuonq09@gmail.com>
2024-03-04 11:55:15 -0600

    finished reading province lots of error on income

--- a2q1.c
+++ a2q1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h> 
+#include <string.h>
 #define MAX_LINE_LENGTH 500 
 struct Province {
     char * provName; 
@@ -9,7 +10,7 @@ struct Province {
 struct Industry 
 {
     int code; 
-    char * indName; 
+    char *indName; 
 };
 
 struct Income
@@ -21,6 +22,11 @@ struct Income
     
 };
 int count_line( const char *filename);
+void make_array_province(int num_lines, const char*filename, struct Province *provinceArr);
+void make_array_income(int num_lines, const char*filename, struct Income *incomeARrr);
+void freeProvinceArr(struct Province *provinceArr, int num_lines);
+void freeIncomeArr(struct Income *incomeArr, int num_lines);
+
 
 int main (void)
 {
@@ -36,16 +42,96 @@ int main (void)
     const char *file_q1_industry = "q1_industry.txt";
     int num_lines_industry = count_line(file_q1_industry)-1; 
 
-    struct Province *provinceArr = (struct Province*)malloc(num_lines_income,sizeof(struct Province)); 
-
+    struct Province *provinceArr = malloc(num_lines_province * sizeof(struct Province)); 
+    struct Income *incomeArr = malloc(num_lines_income * sizeof(struct Income)); 
     // struct array for province
-    make_array_province(num_lines_province,file_q1_province,&provinceArr); 
+    make_array_province(num_lines_province,file_q1_province,provinceArr); 
+    make_array_income(num_lines_income,file_q1_income,incomeArr); 
     for (int i =0; i < num_lines_province; i++)
     {
-        
+        printf("province number %d: %s\n",i,incomeArr[i].prov); 
     }
+    freeProvinceArr(provinceArr,num_lines_province); 
+    freeIncomeArr(incomeArr,num_lines_income); 
 }
+void make_array_income(int num_lines, const char*filename, struct Income *incomeArr)
+{
+    FILE *file = fopen(filename, "r");
+    char line[MAX_LINE_LENGTH]; 
+    int dataIndex = 0; 
 
+    if (file == NULL)
+    {
+        perror("Error opening file"); 
+    }
+    //skip first line and check for NULL 
+    if (fgets(line,sizeof(line),file)==NULL)
+    {
+        perror("File error"); 
+    }
+
+    while(fgets(line,sizeof(line),file) != NULL)
+    {
+        char *token = strtok(line,","); 
+        int tokenIndex = 0; 
+        while(token!=NULL && dataIndex <= num_lines)
+        {
+            switch(tokenIndex)
+            {
+                case 0:
+
+                    incomeArr[dataIndex].prov = malloc(strlen(token) + 1); 
+                    strcpy(incomeArr[dataIndex].prov,token);  
+                    break; 
+                
+                case 1:
+                    char *end; 
+                    //end point to the first char of the string that wasnt a number 
+                    incomeArr[dataIndex].year = strtol(token,&end,10);
+                    if(end==token)
+                    {
+                        perror(printf("no year were found at line %d", dataIndex+2)); 
+                    }
+                    if(*end != '\0')
+                    {
+                        perror(printf("further character exist after year at line: %d", dataIndex+2)); 
+                    }
+                    break; 
+                case 3:
+                    char *end; 
+                    //end point to the first char of the string that wasnt a number 
+                    incomeArr[dataIndex].income = strtol(token,&end,10);
+                    if(end==token)
+                    {
+                        perror(printf("no income were found at line %d", dataIndex+2)); 
+                    }
+                    if(*end != '\0')
+                    {
+                        perror(printf("further character exist after income at line: %d", dataIndex+2)); 
+                    }
+                    break; 
+                case 4: 
+                    char *end; 
+                    //end point to the first char of the string that wasnt a number 
+                    incomeArr[dataIndex].industry = strtol(token,&end,10);
+                    if(end==token)
+                    {
+                        perror(printf("no industry code were found at line %d", dataIndex+2)); 
+                    }
+                    if(*end != '\0')
+                    {
+                        perror(printf("further character exist after industry code at line: %d", dataIndex+2)); 
+                    }
+                    break; 
+            }   
+            tokenIndex++; 
+            token=strtok(NULL,","); 
+        }
+        dataIndex++; 
+    }
+    fclose(file); 
+
+}
 void make_array_province(int num_lines, const char*filename, struct Province *provinceArr)
 {
     FILE *file = fopen(filename, "r"); 
@@ -55,7 +141,6 @@ void make_array_province(int num_lines, const char*filename, struct Province *pr
     if(file == NULL)
     {
         perror("Error opening file");
-        return -1; 
     }
 
 
@@ -63,7 +148,6 @@ void make_array_province(int num_lines, const char*filename, struct Province *pr
     if(fgets(line,sizeof(line),file) == NULL)
     {
         perror("File error");
-        return -1;
     }
     //put every line in file to line
     while(fgets(line,sizeof(line),file) != NULL)
@@ -80,8 +164,9 @@ void make_array_province(int num_lines, const char*filename, struct Province *pr
                     strcpy(provinceArr[dataIndex].provName,token); 
                     break; 
 
+
                 case 1: //2nd part of token will be prov
-                    provinceArr[dataIndex].prov = malloc((token)+1);
+                    provinceArr[dataIndex].prov = malloc(strlen(token)+1);
                     strcpy(provinceArr[dataIndex].prov,token); 
                     break; 
             }
@@ -92,7 +177,7 @@ void make_array_province(int num_lines, const char*filename, struct Province *pr
         }
         dataIndex++; 
     }
-
+    fclose(file); 
 }
 
 
@@ -116,4 +201,36 @@ int count_line( const char *filename)
     }
     fclose(file); 
     return lines; 
+}
+
+void freeProvinceArr(struct Province *provinceArr, int num_lines)
+{
+    for (int i = 0; i < num_lines; i++)
+    {
+        if(provinceArr[i].prov != NULL)
+        {
+            free(provinceArr[i].prov); 
+            provinceArr[i].prov = NULL; 
+        }
+        if(provinceArr[i].provName != NULL)
+        {
+            free(provinceArr[i].provName);
+            provinceArr[i].provName = NULL; 
+        }
+    }
+    free(provinceArr); 
+}
+
+void freeIncomeArr(struct Income *incomeArr, int num_lines)
+{
+    for (int i = 0; i < num_lines; i++)
+    {
+        if(incomeArr[i].prov != NULL)
+        {
+            free(incomeArr[i].prov); 
+            incomeArr[i].prov = NULL; 
+        }
+        
+    }
+    free(incomeArr); 
 }

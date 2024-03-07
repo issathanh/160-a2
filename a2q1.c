1d408e72785b359e418523c2505bb2fcdc1cf8b2
Thanh Thuong <thanh.thuonq09@gmail.com>
2024-03-06 22:26:38 -0600

    added find

--- a2q1.c
+++ a2q1.c
@@ -15,21 +15,20 @@ struct Industry
 
 struct Income
 {
-    struct Province *provArr; //linking the array of province 
     char *incomeProv; 
     int year;
     int income; 
     int industry; 
-    struct Industry *industryLink; 
     
 };
 int count_line( const char *filename);
 void make_array_province(int num_lines, const char*filename, struct Province *provinceArr);
-void make_array_income(int num_lines, const char*filename, struct Income *incomeARrr,struct Province *provinceArr);
+void make_array_income(int num_lines, const char*filename, struct Income *incomeARrr,struct Province *provinceArr,struct Industry *industryArr);
 void freeProvinceArr(struct Province *provinceArr, int num_lines);
 void freeIncomeArr(struct Income *incomeArr, int num_lines);
 void freeIndustryArr(struct Industry *industryArr, int num_lines);
 void make_array_industry (int num_lines, const char*filename, struct Industry * industryArr);
+void find_helper (char* province, int year, char*industry, struct Income *incomeArr,struct Province *provinceArr,struct Industry *industryArr,int province_length, int income_length, int industry_length);
 
 
 int main (void)
@@ -51,17 +50,49 @@ int main (void)
     struct Industry *industryArr = malloc(num_lines_industry*sizeof(struct Industry)); 
     // struct array for province
     make_array_province(num_lines_province,file_q1_province,provinceArr); 
-    make_array_income(num_lines_income,file_q1_income,incomeArr,provinceArr); 
     make_array_industry(num_lines_industry,file_q1_industry,industryArr);
-    for (int i =0; i < num_lines_income; i++)
-    {
-        printf("Industry name %d: %s\n",i,industryArr[i].indName); 
-    }
+    make_array_income(num_lines_income,file_q1_income,incomeArr,provinceArr,industryArr); 
+
+
+    // for (int i =0 ; i < num_lines_income; i++)
+    // {
+    //     printf("%d\n", incomeArr[i].industry);
+    // }
+    find_helper("Ontario", 2017, "Aqua", incomeArr,provinceArr, industryArr,num_lines_province,num_lines_income,num_lines_industry);
     freeProvinceArr(provinceArr,num_lines_province); 
     freeIncomeArr(incomeArr,num_lines_income); 
     freeIndustryArr(industryArr,num_lines_industry); 
 }
 
+// void FIND(char *province, int year, char* Ind)
+// {
+
+// }
+void find_helper (char* province, int year, char*industry, struct Income *incomeArr,struct Province *provinceArr,struct Industry *industryArr,int province_length, int income_length, int industry_length)
+{
+    int income = 0; 
+    int industry_name_len = strlen(industry); 
+    for (int i = 0; i < province_length; i++)
+    {
+        if(strcmp(province,provinceArr[i].provName)==0){
+            for (int j =0; j<industry_length;j++)
+            {
+                if(strncmp(industry,industryArr[j].indName, industry_name_len) == 0)
+                {
+                    for (int k = 0; k < income_length; k++)
+                    {
+                        if(industryArr[j].code == incomeArr[k].industry&&incomeArr[k].year == year)
+                        {
+                           income+=incomeArr[k].income; 
+                        }
+                    }
+                    printf("%s %d",industryArr[j].indName, income); 
+                    income = 0; 
+                }
+            }
+        }
+    }
+}
 void make_array_industry (int num_lines, const char*filename, struct Industry * industryArr)
 {
     FILE *file = fopen(filename, "r"); 
@@ -112,7 +143,7 @@ void make_array_industry (int num_lines, const char*filename, struct Industry * 
     fclose(file); 
 }
 
-void make_array_income(int num_lines, const char*filename, struct Income *incomeArr,struct Province *provinceArr)
+void make_array_income(int num_lines, const char*filename, struct Income *incomeArr,struct Province *provinceArr,struct Industry *industryArr)
 {
     FILE *file = fopen(filename, "r");
     char line[MAX_LINE_LENGTH]; 
@@ -132,7 +163,6 @@ void make_array_income(int num_lines, const char*filename, struct Income *income
 
     while(fgets(line,sizeof(line),file) != NULL)
     {
-        incomeArr[dataIndex].provArr = provinceArr; 
         char *token = strtok(line,","); 
         int tokenIndex = 0; 
         
@@ -155,7 +185,7 @@ void make_array_income(int num_lines, const char*filename, struct Income *income
                         printf("no year were found at line %d", dataIndex+2); 
                     }
                     break; 
-                case 3: //set the income
+                case 2: //set the income
                     //end point to the first char of the string that wasnt a number 
                     incomeArr[dataIndex].income = strtol(token,&end,10);
                     if(end==token)
@@ -164,7 +194,7 @@ void make_array_income(int num_lines, const char*filename, struct Income *income
                     }
                     
                     break; 
-                case 4: //set the industry id
+                case 3: //set the industry id
                     //end point to the first char of the string that wasnt a number 
                     incomeArr[dataIndex].industry = strtol(token,&end,10);
                     if(end==token)
